6c77ea18be6bb8fb693537cc9297ed747dfb4303
Anant Thazhemadam <47104651+thazhemadam@users.noreply.github.com>
2021-07-16 01:34:29 +0530

    Update build.jl

--- deps/build.jl
+++ deps/build.jl
@@ -1,9 +1,8 @@
 using Conda
-using LinearAlgebra
 
 Conda.add("ase", channel = "conda-forge")
 Conda.add("rdkit", channel = "conda-forge")
 Conda.add("pymatgen", channel = "conda-forge")
-if BLAS.vendor() == :mkl
+try
   Conda.rm("mkl")
   Conda.add("mkl")
