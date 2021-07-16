49ed484f8d72b1b36bf7a892c22f540e883d97e1
Anant Thazhemadam <47104651+thazhemadam@users.noreply.github.com>
2021-07-17 03:59:25 +0530

    Update build.jl

--- deps/build.jl
+++ deps/build.jl
@@ -3,9 +3,4 @@ using Conda
 Conda.add("ase", channel = "conda-forge")
 Conda.add("rdkit", channel = "conda-forge")
 Conda.add("pymatgen", channel = "conda-forge")
-try
-    Conda.rm("mkl")
-    Conda.add("mkl")
-finally
-    println("Finished building.")
-end
+Conda.add("mkl")
